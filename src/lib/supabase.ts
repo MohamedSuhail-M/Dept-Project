3228ece1c07e73a70cfe7c8b204bfd8dbdc553c4
MOHAMED SUHAIL M <mohamedsuhail.msivetai@gmail.com>
2026-08-16 19:26:36 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const url = import.meta.env.VITE_SUPABASE_URL;
-const anonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const url = import.meta.env.VITE_SUPABASE_URL || '';
+const anonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
+
+if (!url || !anonKey) {
+  console.warn('Missing Supabase environment variables! Check your Vercel Project Settings.');
+}
 
 export const supabase = createClient(url, anonKey);
